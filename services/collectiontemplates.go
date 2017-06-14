15c6ae0c615594a19e5e5e940b5335fd6a77084d
Dennis Bappert <bappert@outlook.com>
2017-06-14 21:20:30 +0200

    start implementation on templates

--- services/collectiontemplates.go
+++ services/collectiontemplates.go
@@ -1,10 +1,14 @@
 package services
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
+	"os"
 
 	"path"
+
+	"strings"
 
 	"github.com/dennisbappert/fileharbor/common"
 	"github.com/jmoiron/sqlx"
@@ -21,6 +25,12 @@ type templateDefinition struct {
 	IncludeContentTypes []string `json:"include_contenttypes"`
 }
 
+type CollectionTemplate struct {
+	Title       string
+	Description string
+	Language    string
+}
+
 func NewCollectionTemplateService(configuration *common.Configuration, database *sqlx.DB, services *Services) *CollectionTemplateService {
 	service := &CollectionTemplateService{Service{database: database, configuration: configuration, Services: services}}
 	return service
@@ -28,6 +38,12 @@ func NewCollectionTemplateService(configuration *common.Configuration, database 
 
 func (service *CollectionTemplateService) GetAvaliableTemplates(language string) error {
 	log.Println("searching all avaliable templates in template folder", service.configuration.TemplateFolder)
+
+	// we only want to work with lower-case names
+	language = strings.ToLower(language)
+
+	// the result set
+	var results []CollectionTemplate
 
 	templatePath := service.configuration.TemplateFolder
 	entries, err := ioutil.ReadDir(templatePath)
@@ -51,11 +67,57 @@ func (service *CollectionTemplateService) GetAvaliableTemplates(language string)
 		}
 
 		for _, entry := range files {
-			file := path.Join(folder, entry.Name())
+			filePath := path.Join(folder, entry.Name())
 
-			log.Println("processing file", file)
+			// normalizing the filename
+			fileName := strings.ToLower(entry.Name())
+
+			if strings.HasPrefix(fileName, "template.") {
+				log.Println("found template definition file", filePath)
+				fileLanguage := strings.Replace(fileName, "template.", "", 1)
+				fileLanguage = strings.Replace(fileLanguage, ".json", "", 1)
+
+				log.Println("file has the following language", fileLanguage)
+
+				if language == fileLanguage {
+					// we found a matching template
+					log.Println("found a matching template for given language", filePath)
+
+					// try opening and parsing the json file
+					file, err := os.Open(filePath)
+
+					if err != nil {
+						log.Println("unable to open file - skipping...", err)
+						continue
+					}
+
+					templateDefinition := &templateDefinition{}
+					decoder := json.NewDecoder(file)
+					err = decoder.Decode(&templateDefinition)
+
+					if err != nil {
+						log.Println("unable to parse file - invalid template - skipping...", err)
+						continue
+					}
+
+					log.Println("loaded template", templateDefinition)
+
+					// parsing site columns
+					for _, include := range templateDefinition.IncludeColumns {
+						log.Println("processing include", include)
+						includePath := path.Join(folder, include)
+						log.Println("included file is", includePath)
+					}
+
+					//results = append(results, *result)
+				}
+			}
+
+			log.Println("finished processing file", filePath)
 		}
 	}
+
+	log.Println(results)
 
 	return nil
 }
