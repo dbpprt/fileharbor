0c093d5cf1037592d5b57dfedc8056da4275a8e6
Dennis Bappert <bappert@outlook.com>
2017-09-04 21:35:59 +0200

    remove go prototype

--- loadtest.ps1
+++
@@ -1,9 +0,0 @@
-﻿$cmd = {
-  for ($i = 0; $i -lt 5000; $i++) { X:\Development\Go\src\github.com\dennisbappert\fileharbor\tests.ps1 }
-}
-
-$foo = "foo"
-
-1..25 | ForEach-Object {
-  Start-Job -ScriptBlock $cmd -ArgumentList $_, $foo
-}
--- models/usercollectionmapping.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type UserCollectionMapping struct {
-	UserId       string `db:"user_id"`
-	CollectionId string `db:"collection_id"`
-	IsDefault    bool   `db:"is_default"`
-}
